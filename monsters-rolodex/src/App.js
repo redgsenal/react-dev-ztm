52a4295bed8b0a94b3c6fe11baad8f2a212d1a83
Reggie <reggie.senal@gmail.com>
2024-09-16 14:29:28 +0800

    update state with object

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -12,10 +12,10 @@ class App extends Component {
     // state is always a JSOn object
     // key: value form
     this.state = {
-      name: 'Reggie',
-      greetings: 'Have a nice day!'
+      name: { firstName: 'Reggie', lastName: 'Senal' },
+      greetings: 'Have a nice day!',
+      company: 'ACME Co.'
     }
-
   }
 
   // render() function tells what is the content
@@ -26,12 +26,13 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <p>Hello There!!!</p>
           <p>{this.state.greetings}</p>
-          <p>Hi {this.state.name}</p>
+          <p>Hi {this.state.name.firstName} {this.state.name.lastName}</p>
+          <p>Welcome to {this.state.company}</p>
           <button onClick={() => {
             // any changes to the objects state will trigger virtual dom updates
             // setState will look at the state object and look for the key-value changes
             this.setState({
-              name: 'James',
+              name: { firstName: 'James', lastName: 'Jones' },
               greetings: 'Good day!'
             });
             console.log(this.state);
