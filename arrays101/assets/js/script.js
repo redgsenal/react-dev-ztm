73da7ddd55a807cd625358d4a08056a381df93fa
Reggie <reggie.senal@gmail.com>
2024-09-17 07:41:01 +0800

    arrays 101

---
+++ arrays101/assets/js/script.js
@@ -0,0 +1,15 @@
+const myArray = [1, 2, 3, 4, 5];
+// map()
+// console.log('hello');
+// const myArray = [1, 2, 3, 4, 5];
+// console.log(myArray);
+// const m2 = myArray.map(n => n + 1);
+// const m3 = myArray.map(n => "w");
+// console.log(m2);
+// console.log(m3);
+
+// filter
+const f1 = myArray.filter(n => n < 4);
+const f2 = myArray.filter(n => !(n < 4));
+console.log('f1:', f1);
+console.log('f2:', f2);
