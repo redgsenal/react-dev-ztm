76c73626fcbaf17237e84ec39bda249a50af0d6b
Reggie <reggie.senal@gmail.com>
2024-09-17 06:44:26 +0800

    update set state with callback

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -28,15 +28,40 @@ class App extends Component {
           <p>{this.state.greetings}</p>
           <p>Hi {this.state.name.firstName} {this.state.name.lastName}</p>
           <p>Welcome to {this.state.company}</p>
-          <button onClick={() => {
+          <button
             // any changes to the objects state will trigger virtual dom updates
             // setState will look at the state object and look for the key-value changes
-            this.setState({
-              name: { firstName: 'James', lastName: 'Jones' },
-              greetings: 'Good day!'
-            });
-            console.log(this.state);
-          }}>
+            // from
+            // onClick={() => {
+            // this.setState(
+            // {
+            // name: { firstName: 'James', lastName: 'Jones' },
+            // greetings: 'Good day!'
+            // }
+            // console.log(this.state);
+            // );
+            // the state might not be updated at this point
+            // cos state is an asynchronous; meaning at this point it might o not already change
+            // the chnage value is still ongoing;
+            // solution: pass it as a callback or as a function
+            onClick={() => {
+              this.setState(
+                // state parameter
+                (state, prop) => {
+                  console.log('state:', state);
+                  console.log('prop:', prop);
+                  // return and shallow merge to the this.state object
+                  return {
+                    name: { firstName: 'James', lastName: 'Jones' },
+                    greetings: 'Good day!'
+                  };
+                },
+                // callback function after the state updated
+                () => {
+                  console.log(this.state);
+                }
+              );
+            }}>
             Change Name
           </button>
         </header>
