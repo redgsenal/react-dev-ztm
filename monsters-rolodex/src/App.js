b18d263495531814f9edf3a8743520abe94e357a
Reggie <reggie.senal@gmail.com>
2024-09-17 18:59:39 +0800

    show how react render sequence, constructor, render and mount (and render again)

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -4,19 +4,24 @@ import './App.css';
 // converted to class Component
 class App extends Component {
   // initial function run first
+  // constructors always runs first
   constructor() {
     // calls the constructor of the parent component class
     super();
 
     // state is always a JSOn object
     // key: value form
+    // probably most of the time, set the state
     this.state = {
       monsters: []
     }
+    console.log('constructor: ', 1);
   }
 
   // this method is called whenever the component is mounted; 1st time rendered on the page / DOM; run once
+  // after render, 3rd to run is componentDidMount is called next (life cycles)
   componentDidMount() {
+    console.log('componentDidMount: ', 3);
     // a promise called; see Appendix section
     fetch('https://jsonplaceholder.typicode.com/users')
       .then((response) => response.json())
@@ -28,7 +33,11 @@ class App extends Component {
   }
 
   // render() function tells what is the content
+  // render is called 2nd after constructor
+  // render what to show (mount this)
+  // this might call again whenever the state changes
   render() {
+    console.log('render: ', 2);
     return (
       <div className="App">
         {
@@ -40,5 +49,6 @@ class App extends Component {
       </div>
     );
   }
+
 }
 export default App;
