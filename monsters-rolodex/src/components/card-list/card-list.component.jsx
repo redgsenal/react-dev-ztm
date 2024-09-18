ae27875cd4037ddd52608ff593dd1936cf5dc153
Reggie <reggie.senal@gmail.com>
2024-09-18 10:01:53 +0800

    add comments

--- monsters-rolodex/src/components/card-list/card-list.component.jsx
+++ monsters-rolodex/src/components/card-list/card-list.component.jsx
@@ -1,3 +1,6 @@
+// notice how the author names this component structure
+// there should only be one parent component per component
+// multiple components per child component is discourage
 import { Component } from "react";
 
 class CardList extends Component {
