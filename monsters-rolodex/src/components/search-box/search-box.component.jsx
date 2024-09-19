cc8f608611613d4d82cd9d007e8fb09d22196837
Reggie <reggie.senal@gmail.com>
2024-09-19 08:42:29 +0800

    add css to search box

--- monsters-rolodex/src/components/search-box/search-box.component.jsx
+++ monsters-rolodex/src/components/search-box/search-box.component.jsx
@@ -1,10 +1,13 @@
 import { Component } from "react";
+// this css will be bundled with App.css and will be available for the whole site
+// this will be easier to code CSS that is specific for this component
+import "./search-box.styles.css";
 
 class SearchBox extends Component {
 	render() {
 		return (
 			<input
-				className={this.props.className}
+				className={`monsters-search-box ${this.props.className}`}
 				type="search"
 				placeholder={this.props.placeHolder}
 				onChange={this.props.onChangeHandler}
