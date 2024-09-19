beef57637ba249dfdd553fe45d371c705c405af6
Reggie <reggie.senal@gmail.com>
2024-09-19 09:13:09 +0800

    create search component and card list component with each css

--- monsters-rolodex/src/components/card-list/card-list.component.jsx
+++ monsters-rolodex/src/components/card-list/card-list.component.jsx
@@ -3,17 +3,24 @@
 // multiple components per child component is discourage
 // this is a child component and renders any child component if any
 import { Component } from "react";
+import "./card-list.styles.css";
 
 class CardList extends Component {
 	render() {
 		console.log("card list render with props:", this.props);
 		const { monsters } = this.props;
-
 		return (
-			<div>
-				{monsters.map((monster) => (
-					<h1 key={monster.id}>{monster.name}</h1>
-				))}
+			<div className="card-list">
+				{monsters.map((monster) => {
+					const { id, name, email } = monster;
+					return (
+						<div className="card-container" key={id}>
+							<img alt={`monster ${name}`} src={`https://robohash.org/${id + 99}?set=set2&size=180x180`} />
+							<h2>{name}</h2>
+							<a href={`mailto:${email}`}>{email}</a>
+						</div>
+					);
+				})}
 			</div>
 		);
 	}
