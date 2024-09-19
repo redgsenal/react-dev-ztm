0be97e5c87fbe511977b1f6ac6ab77c169651f7a
Reggie <reggie.senal@gmail.com>
2024-09-19 21:46:25 +0800

    update converted to function version

--- monsters-rolodex-v2/src/components/card-list/card-list.component.jsx
+++ monsters-rolodex-v2/src/components/card-list/card-list.component.jsx
@@ -2,27 +2,19 @@
 // there should only be one parent component per component
 // multiple components per child component is discourage
 // this is a child component and renders any child component if any
-import { Component } from "react";
 import CardMonster from "../card-monster/card-monster.component.jsx";
 import "./card-list.styles.css";
 
-class CardList extends Component {
-	render() {
-		console.log("card list render with props:", this.props);
-		const { monsters } = this.props;
-		return (
-			<div className="card-list">
-				{monsters.map((monster) => {
-					const { id, name, email } = monster;
-					return (
-						<div className="card-container" key={id}>
-							<CardMonster id={id} name={name} email={email} />
-						</div>
-					);
-				})}
-			</div>
-		);
-	}
-}
+const CardList = ({ monsters }) => (
+	<div className="card-list">
+		{monsters.map((monster) => {
+			return (
+				<div className="card-container" key={monster.id}>
+					<CardMonster monster={monster} />
+				</div>
+			);
+		})}
+	</div>
+);
 
 export default CardList;
--- monsters-rolodex-v2/src/components/search-box/search-box.component.jsx
+++ monsters-rolodex-v2/src/components/search-box/search-box.component.jsx
@@ -1,19 +1,14 @@
-import { Component } from "react";
 // this css will be bundled with App.css and will be available for the whole site
 // this will be easier to code CSS that is specific for this component
 import "./search-box.styles.css";
 
-class SearchBox extends Component {
-	render() {
-		return (
-			<input
-				className={`monsters-search-box ${this.props.className}`}
-				type="search"
-				placeholder={this.props.placeHolder}
-				onChange={this.props.onChangeHandler}
-			/>
-		);
-	}
-}
+const SearchBox = ({ className, placeHolder, onChangeHandler }) => (
+	<input
+		className={`monsters-search-box ${className}`}
+		type="search"
+		placeholder={placeHolder}
+		onChange={onChangeHandler}
+	/>
+);
 
 export default SearchBox;
