2ad38bca34c73eb4816443682a923c70d2cd94c0
Reggie <reggie.senal@gmail.com>
2024-09-20 10:43:38 +0800

    update list with keys

--- pure-react/assets/js/script.js
+++ pure-react/assets/js/script.js
@@ -1,17 +1,27 @@
 const Person = props => {
-    return React.createElement('div', {}, [
+    return React.createElement('li', { key: props.key, className: 'person-item', 'data-key': props.key }, [
         React.createElement('h1', { className: 'person-title', 'data-foo': 'foo-1' }, props.name),
         React.createElement('p', { className: 'person-occupation', 'data-bar': 'bar-1' }, props.occupation)
     ]);
 }
 
+const persons = [
+    { key: 1, name: 'John', occupation: 'carpenter' },
+    { key: 2, name: 'Jill', occupation: 'clerk' },
+    { key: 3, name: 'Kaye', occupation: 'police' }
+]
+
+const createPersonElements = (persons) => {
+    return React.createElement('ul', {}, persons.map((person, i) => {
+        return React.createElement(Person, { key: person.key, name: person.name, occupation: person.occupation })
+    }));
+}
+
 const App = () => {
     return React.createElement('div', {}, [
-        React.createElement('h1', { className: 'title' }, "React is rendered"),
-        React.createElement(Person, { name: 'John', occupation: 'carpenter' }, null),
-        React.createElement(Person, { name: 'Jill', occupation: 'clerk' }, null),
-        React.createElement(Person, { name: 'Kaye', occupation: 'police' }, null),
+        React.createElement('h1', { className: 'title' }, "React is rendered"), createPersonElements(persons)
     ]);
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(React.createElement(App));
