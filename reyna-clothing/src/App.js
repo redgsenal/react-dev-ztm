509ba4ff353bcfed2495c850918f9e868460c574
Reggie <reggie.senal@gmail.com>
2024-09-21 20:37:45 +0800

    nested routes

--- reyna-clothing/src/App.js
+++ reyna-clothing/src/App.js
@@ -2,10 +2,16 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './routes/home/home.component.jsx';
 import './assets/styles/app.styles.scss';
 
+const Shop = () => {
+  return <h1>This is the Shop</h1>;
+};
+
 const App = () => {
   return (
     <Routes>
-      <Route path='/' element={<Home />} />
+      <Route path='/home' element={<Home />} >
+        <Route path='shop' element={<Shop />} />
+      </Route>
     </Routes>
   );
 };
--- reyna-clothing/src/routes/home/home.component.jsx
+++ reyna-clothing/src/routes/home/home.component.jsx
@@ -1,4 +1,6 @@
+import { Outlet } from 'react-router-dom'
 import Directory from '../../components/directory/directory.component'
+import './home.styles.scss'
 
 const Home = () => {
     const categories = [
@@ -28,7 +30,12 @@ const Home = () => {
             imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
         },
     ]
-    return <Directory categories={categories} />
+    return (
+        <div>
+            <Directory categories={categories} />
+            <Outlet />
+        </div>
+    )
 }
 
 export default Home
