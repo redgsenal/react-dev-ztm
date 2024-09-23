85d8af9693e0031d004a07f48b81a353e2761e19
Reggie <reggie.senal@gmail.com>
2024-09-23 08:51:20 +0800

    configure firebase db and add user data

--- reyna-clothing/src/utils/firebase/firebase.utils.js
+++ reyna-clothing/src/utils/firebase/firebase.utils.js
@@ -6,6 +6,13 @@ import {
     GoogleAuthProvider
 } from 'firebase/auth'
 
+import {
+    getFirestore,
+    doc,
+    getDoc,
+    setDoc
+} from 'firebase/firestore'
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
     apiKey: "AIzaSyB4oJwZ2M2dldcJoWW0iEQV7Yz1Jt5M8S0",
@@ -25,3 +32,25 @@ provider.setCustomParameters({
 
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+export const createUserDocumentFromAuth = async (userAuth) => {
+    const userDocRef = doc(db, 'users', userAuth.uid);
+    console.log(userDocRef);
+    const userSnapshot = await getDoc(userDocRef);
+    console.log(userSnapshot.exists());
+
+    // check if data not exists
+    if (!userSnapshot.exists()) {
+        // create / set data from userAuth to collection
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+        try {
+            await setDoc(userDocRef, { displayName, email, createdAt });
+        } catch (error) {
+            console.log('error creating user data: ', error.message)
+        }
+    }
+
+    return userDocRef;
+}
